0a0ceda103ee5f92465d7c43822f935205df1eef
Tushar <tusharm567@gmail.com>
2016-10-06 14:47:13 +0530

    Added web speech api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendfile(path.resolve('public/static/web-speech-api/index.html'));
 });
 
 module.exports = router;
